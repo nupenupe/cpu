f7b3b9094a04eb195a14c5b62e519c126f483036
nupenupe <66927117+nupenupe@users.noreply.github.com>
2021-02-09 19:44:03 +0900

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,9 +1,9 @@
 
-module alu( reset, tclk, instruction, state, acc, latch, c, z, d_bus );
+module alu( reset, tclk, instruction, ldAcc, useAlu, dbusSelect, acc, latch, c, z, d_bus );
 
 input reset, tclk; //reset ,clock
 input [7:0] instruction; //instruction register
-input [1:0] state; //state (instrucrtion cycle)
+input ldAcc, useAlu, dbusSelect;
 output [7:0] acc, latch; //accumulator,latch
 reg [7:0] acc, latch;
 output c,z; //carry_flag ,zero_flag
@@ -15,7 +15,7 @@ always @(posedge tclk or posedge reset) begin
         acc<=8'b0; latch<=8'b0; c<=1'b0; z<=1'b0; 
     end
     else begin
-    if(((instruction[7:5] == 3'b0xx)||(instruction[7:5] == 3'b101))&&(state == EXEC_B)) begin //EXEC_Bの時,算術命令とSTを行う
+    if(useAlu) begin //EXEC_Aの時,算術命令とSTを行う
         casex(instruction[7:5])
             3'b000: begin//ADD 
                 {c,latch}<=acc + d_bus;
@@ -30,17 +30,22 @@ always @(posedge tclk or posedge reset) begin
                 z<=((~(acc & d_bus))==8'b0);
                 end
             3'b011: begin //SHIFT
-                if(instruction[4:0]==5'h1f) {c,latch}<=acc >> 1; z<=((acc >>1 )==8'b0);//RIGHT SHIFT
-                else {c,latch}<=acc << 1; z<=((acc <<1 )==8'b0); //LEFT SHIFT
+                if(instruction[4:0]==5'h1f)begin //RIGHT SHIFT
+                {c,latch}<=acc >> 1; 
+                z<=((acc >>1 )==8'b0);
+                end
+                else begin {c,latch}<=acc << 1; //LEFT SHIFT
+                z<=((acc <<1 )==8'b0); 
+                end
                 end
             3'b101: latch<=acc; //ST
         endcase
     end
-    if((instruction[7:5] == 3'b100) && (state == EXEC_A)) acc<=d_bus; //EXEC_Aの時LD行う
+    if(ldAcc) acc<=d_bus; //EXEC_Aの時LD行う
     end
 end
 
-assign d_bus= (((instruction[7:5] == 3'b0xx)||(instruction[7:5] == 3'b100))&&(state == EXEC_B))? latch: 8'bz; //EXEC_Bの時latchの値をd_busへ流すor流さない(ハイインピーダンス) 
+assign d_bus= (dbusSelect)? latch: 8'bz; //EXEC_Bの時latchの値をd_busへ流すor流さない(ハイインピーダンス) 
 
 parameter FETCH = 2'b00, DECODE = 2'b01, EXEC_A = 2'b10, EXEC_B = 2'b11;
 
