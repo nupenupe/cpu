1be2a68602c47de389453d9a809d7f03adbf1187
nupenupe <66927117+nupenupe@users.noreply.github.com>
2021-02-10 17:27:12 +0900

    Update alu_test.v

--- alu_test.v
+++ alu_test.v
@@ -1,66 +1,96 @@
 `timescale 1ns/1ns
-module mem_test;
+module alu_test;
+
 //input signal (clock)
 reg tclk = 1;
 always #(STEP/2) tclk<=!tclk;
 
 //input signal
-reg [4:0] a_bus; 
-reg [7:0] inp; 
-reg [7:0] instruction; 
-reg dbusSelect; 
-reg [7:0] in_d_bus;
+reg reset;
+reg [7:0] instruction;
+reg ldAcc, useAlu, dbusSelect;
+reg [7:0] in_d_bus; //d_busの中身を変えるための変数
+
 
 //output siganl
-wire [7:0] oup;
+wire c,z;
+wire [7:0] acc,latch;
 wire [7:0] d_bus;
-wire [7:0] view_mem;
-//inout信号の入力をそのまま出力に流すか(d_busの中身を変えるため)
-reg inout_flag ;
+
+//inout信号の入力をそのまま出力に流すか(Execute cycle Aの時にd_busの中身を変えるため)
+reg inout_flag = 1;
 
 //双方向信号対応用
 assign d_bus = (inout_flag)? in_d_bus: 8'bz;
 
+
+
 parameter STEP = 100;
 
-mem mem( tclk, a_bus, inp, oup, instruction, dbusSelect, d_bus, view_mem );
+alu alu( reset, tclk, instruction, ldAcc, useAlu, dbusSelect, acc, latch, c, z, d_bus );
 
     initial begin
 
-        instruction=8'b010xxxxx; inp = 8'h0; dbusSelect = 1'b1; 
-        in_d_bus = 8'bz; inout_flag = 1'b0;
+        reset=1'b0; instruction=8'b01100000; ldAcc = 1'b0; useAlu = 1'b0; dbusSelect = 1'b0; 
+        in_d_bus = 8'bz;
 
         //正常動作の確認
-            //0~4番地までを読みだしてd_busに流す
-            #STEP dbusSelect = 1'b0; a_bus = 4'd0;
-            #STEP a_bus = 4'd1;
-            #STEP a_bus = 4'd2;
-            #STEP a_bus = 4'd3;
-            #STEP a_bus = 4'd4;
-            //30番地(input)をd_busに流す
-            #STEP a_bus = 30; inp=8'b11111111;
-            //LD命令の時
-                //d_busの内容をoutputするとき
-                #STEP instruction = 8'b10000000; in_d_bus = 8'b01010101; a_bus = 31; inout_flag = 1'b1; dbusSelect = 1'b1;
-                //d_busの内容をRAMに書き込むとき
-                #STEP a_bus = 5; 
+            //Fetch cycle　ALU付近は何もしない
+            #STEP instruction =8'b01101000;
+            #STEP
+
+            //reset
+            #STEP reset = 1'b1;
+            #STEP reset = 1'b0;
+
+            //Execute cycle A
+                //LD命令
+                #STEP in_d_bus = 8'b10011110; ldAcc = 1'b1;
+                //ADD命令
+                #STEP ldAcc = 1'b0; useAlu = 1'b1; in_d_bus = 8'b01100001; instruction = 8'b000xxxxx;
+                //SUB命令
+                #STEP instruction = 8'b001xxxxx; in_d_bus = 8'b10011110; 
+                //NAND命令
+                #STEP instruction = 8'b010xxxxx; in_d_bus = 8'b10101010; 
+                //SHIFT命令
+                #STEP instruction = 8'b01100000; //LEFT SHIFT
+                #STEP instruction = 8'b01111111; //RIGHT SHIFT
+                //ST命令
+                #STEP instruction = 8'b101xxxxx;
+
+                #STEP useAlu = 1'b0; 
+            
+            //Execute cycle B
+                #STEP in_d_bus = 8'b11111111; inout_flag = 1'b0;
+                #STEP dbusSelect = 1'b1;
+                #STEP dbusSelect = 1'b0;
         
         //異常入力の時の動作
-            //制御信号がおかしい時
-            #STEP dbusSelect = 1'bx; inout_flag = 1'b0; instruction=8'b010xxxxx;
-            #STEP dbusSelect = 1'bz;
+            //reset
+            #STEP reset = 1'bx;
+            #STEP reset = 1'bz;
+            #STEP reset = 1'b1;
+            #STEP reset = 1'b0;
 
-            //番地がおかしい時にどうd_busにながれるか
-            #STEP dbusSelect = 1'b0; a_bus = 4'b000x; inout_flag = 1'b0; dbusSelect = 1'b0;
-            #STEP a_bus = 4'b000z;
+            //Execute cycle A
+                #STEP useAlu = 1'b1; inout_flag = 1'b1;
+                #STEP instruction = 8'bxxxxxxxx;
+                #STEP instruction = 8'bzzzzzzzz;
+                #STEP useAlu = 1'bx;
+                #STEP instruction = 8'b001xxxxx;
+                #STEP useAlu = 1'bz;
+                #STEP instruction = 8'b001xxxxx;
 
+            //Execute cycle B
+                #STEP dbusSelect = 1'bx; inout_flag = 1'b0; useAlu = 1'b0;
+                #STEP dbusSelect = 1'bz;
 
         #STEP $finish;
     end
 
     initial begin
-        $dumpfile("mem_out.vcd");
-        $dumpvars(0, mem_test);
+        $dumpfile("alu_out.vcd");
+        $dumpvars(0, alu_test);
     end
 
 
